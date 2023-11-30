3ffcd1ab2a3a8950a2b96745897aec80e4f359d0
Rizana@2001 <rizanaalam@gmail.com>
2023-11-30 15:29:11 +0530

    add

--- todo/app/views.py
+++ todo/app/views.py
@@ -12,14 +12,29 @@ def index(request):
 
     if request.method=="POST":
         if 'save' in request.POST:
-            
-         todo_form=Todo_form(request.POST)
-         todo_form.save()
+            key=request.POST.get('save')
 
+            if not key:
+              todo_form=Todo_form(request.POST)
+            else:
+              todo_edit=Todo.objects.get(id=key)
+              todo_form=Todo_form(request.POST,instance=todo_edit)
+
+            todo_form.save()
+            todo_form=Todo_form()
+           
         elif 'delete' in request.POST:
            key=request.POST.get('delete')
            todo_del=Todo.objects.get(id=key)
            todo_del.delete()
+
+
+        elif 'edit' in request.POST:
+           key=request.POST.get('edit')
+           todo_edit=Todo.objects.get(id=key)
+           todo_form=Todo_form(instance=todo_edit)
+
+
            
         
 
